8021637467c39fae5d241a947d6d84704ef4c625
psampaz <psampaz@gmail.com>
2020-05-18 19:28:09 +0300

    Update main_test.go

--- snippets/how-to-filter-a-slice/main_test.go
+++ snippets/how-to-filter-a-slice/main_test.go
@@ -7,44 +7,44 @@ import (
 )
 
 // FilterSlice filters a slice based on a predicate
-func FilterSlice(a []int, keep func(x int) bool) []int {
-	if len(a) == 0 {
-		return a
+func FilterSlice(s []int, keep func(x int) bool) []int {
+	if len(s) == 0 {
+		return s
 	}
 
 	n := 0
-	for _, v := range a {
+	for _, v := range s {
 		if keep(v) {
-			a[n] = v
+			s[n] = v
 			n++
 		}
 	}
 
-	return a[:n]
+	return s[:n]
 }
 
 func TestFilterInt(t *testing.T) {
 	tests := []struct {
 		name string
-		a    []int
+		s    []int
 		keep func(x int) bool
 		want []int
 	}{
 		{
 			name: "nil slice",
-			a:    nil,
+			s:    nil,
 			keep: func(int) bool { panic("not implemented") },
 			want: nil,
 		},
 		{
 			name: "empty slice",
-			a:    []int{},
+			s:    []int{},
 			keep: func(int) bool { panic("not implemented") },
 			want: []int{},
 		},
 		{
 			name: "non empty slice",
-			a:    []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			s:    []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			keep: func(x int) bool {
 				return x < 5
 			},
@@ -53,7 +53,7 @@ func TestFilterInt(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := FilterSlice(tt.a, tt.keep); !reflect.DeepEqual(got, tt.want) {
+			if got := FilterSlice(tt.s, tt.keep); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("FilterSlice() = %v, want %v", got, tt.want)
 			}
 		})
